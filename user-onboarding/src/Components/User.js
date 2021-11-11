1927116eb42633ef5d9180971a81f968b7533575
jcpcabanada <jcpcabanada@gmail.com>
2021-11-10 23:15:02 -0800

    pain

--- user-onboarding/src/Components/User.js
+++ user-onboarding/src/Components/User.js
@@ -3,11 +3,12 @@ import React from "react";
  function User ( {users} )  {
      // <h2>{users.last_name}, {users.first_name} </h2>
      // <img>{users.avatar}</img>
+     // <p>Email: {users.email}</p>
+     // <p>Password: {users.password}</p>
     return (
         <div className='container'>
 
-            <p>Email: {users.email}</p>
-            <p>Password: {users.password}</p>
+            <h2>test</h2>
 
         </div>
     )
